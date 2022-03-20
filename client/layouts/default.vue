0dd05b7f5918879ff315228218dbb2c30ad7c27e
squak <squakrazv@gmail.com>
2022-03-20 16:06:31 +0300

    feat: update Vuetify to 3.0.0-beta.0 version

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <VApp>
     <VAppBar color="grey-darken-4">
-      <VImg src="/logo.png" max-height="50" max-width="50" class="ml-2"></VImg>
+      <VImg src="/logo.png" max-height="50" max-width="50" class="mr-2"></VImg>
       <VToolbarTitle>Prolog lab</VToolbarTitle>
     </VAppBar>
     <slot></slot>
